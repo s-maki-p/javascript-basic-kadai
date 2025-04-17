b5d547cad447931b9fa7f82c5f2cd850d70964e7
s-maki-p <sugar.maki.3316@gmail.com>
2025-04-17 16:30:34 +0900

    修正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,7 @@
 //身長・体重を設定
-const weight = 68;
-const height = 1.7;
+let weight = 68;
+let height = 1.7;
+let bmi = weight / (height * height)
 
 //計算・出力
-console.log(weight / (height * height));
+console.log(bmi);
